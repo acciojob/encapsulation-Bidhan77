a6dbf030052bb3d8e6c93b8703385393ffc650d5
Bidhan <bidhanrakshitn@gmail.com>
2023-03-15 14:44:16 +0530

    Assignment Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args){
+
+    RWOnly obj = new RWOnly();
+
+    obj.setName("Bidhan");
+
+    String ans = obj.getName();
+
+    System.out.println(ans);
+}
+
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class RWOnly {
-    
+
+    private String name;
+
+    // Getter
+
+    public String getName(){
+        return name;
+    }
+
+    // Setter
+
+    public void setName(String newName){
+        this.name = newName;
+
+    }
+
+
 }
